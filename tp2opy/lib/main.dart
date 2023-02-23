c62fbc4765440b3d9d03b8aeebb71a4c686f2d88
Angelique_Mrcn <angelique64.merceron@gmail.com>
2023-02-23 19:24:38 +0100

    print image marche pas encore

---
+++ tp2opy/lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+// import 'util.dart';
+
+
+
+void main() {
+  const Image(image: NetworkImage('https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg'));
+}
