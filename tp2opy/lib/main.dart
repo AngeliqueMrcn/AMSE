eb1c6d8cdf51f08f2199a5f40dc887c2b484f9cf
Angelique_Mrcn <angelique64.merceron@gmail.com>
2023-02-27 18:04:17 +0100

    sacffold marche pas

--- tp2opy/lib/main.dart
+++ tp2opy/lib/main.dart
@@ -1,207 +1,55 @@
-
-void main() => runApp(DisplayImageWidget());
-
 import 'package:flutter/material.dart';
+import 'dart:math';
 
-class Tile {
-  String? image;
-  Alignment? alignment;
+void main() => runApp(MyApp());
 
-  Tile({this.image, this.alignment});
-
-  Widget croppedImageTile() {
-    return FittedBox(
-      fit: BoxFit.fill,
-      child: ClipRect(
-        child: Container(
-          child: Align(
-            alignment: this.alignment,
-            widthFactor: 0.3,
-            heightFactor: 0.3,
-            child: Image.network(this.image),
-          ),
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home : Scaffold(
+      appBar: AppBar(
+          title: Text('Display image'),
         ),
-      ),
-    );
-  }
-}
-
-Tile tile = new Tile(
-    image: '/home/angelique_mrcn/Documents/AMSE/tp2opy/assets/images/pic2.jpg', alignment: Alignment(0, 0));
-
-class DisplayImageWidget extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Display a Tile as a Cropped Image'),
-        centerTitle: true,
-      ),
-      body: Center(
-          child: Column(children: [
-        SizedBox(
-            width: 150.0,
-            height: 150.0,
-            child: Container(
-                margin: EdgeInsets.all(20.0),
-                child: this.createTileWidgetFrom(tile))),
-        Container(
-            height: 200,
-            child: Image.network('https://picsum.photos/512',
-                fit: BoxFit.cover))
-      ])),
-    );
-  }
-
-  Widget createTileWidgetFrom(Tile tile) {
-    return InkWell(
-      child: tile.croppedImageTile(),
-      onTap: () {
-        print("tapped on tile");
-      },
-    );
-  }
-}
-
-
-/*
-EXO 6 animation tuile
-import 'package:flutter/material.dart';
-import 'dart:math' as math;
-
-// ==============
-// Models
-// ==============
-
-math.Random random = new math.Random();
-
-class Tile {
-  Color color = Colors.green[400]!;
-
-  Tile(this.color);
-  Tile.randomColor() {
-    this.color = Color.fromARGB(
-        255, random.nextInt(255), random.nextInt(255), random.nextInt(255));
-  }
-}
-
-// ==============
-// Widgets
-// ==============
-
-class TileWidget extends StatelessWidget {
-  final Tile tile;
-
-  TileWidget(this.tile);
-
-  @override
-  Widget build(BuildContext context) {
-    return this.coloredBox();
-  }
-
-  Widget coloredBox() {
-    return Container(
-        color: tile.color,
-        child: Padding(
-          padding: EdgeInsets.all(70.0),
-        ));
-  }
-}
-
-void main() => runApp(new MaterialApp(home: PositionedTiles()));
-
-class PositionedTiles extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() => PositionedTilesState();
-}
-
-class PositionedTilesState extends State<PositionedTiles> {
-  List<Widget> tiles =
-      List<Widget>.generate(2, (index) => TileWidget(Tile.randomColor()));
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Moving Tiles'),
-        centerTitle: true,
-      ),
-      body: Row(children: tiles),
-      floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.sentiment_very_satisfied), onPressed: swapTiles),
-    );
-  }
-
-  swapTiles() {
-    setState(() {
-      tiles.insert(1, tiles.removeAt(0));
-    });
-  }
-}
-*/
-
-
-
-
-
-/*
-class Tile {
-  String? image;
-  Alignment? alignment;
-
-  Tile({this.image, this.alignment});
-
-  Widget croppedImageTile() {
-    return FittedBox(
-      fit: BoxFit.fill,
-      child: ClipRect(
-        child: Container(
-          child: Align(
-            alignment: this.alignment,
-            widthFactor: 0.3,
-            heightFactor: 0.3,
-            child: Image.network(this.image),
+      body : Text('Display image'),
+      GridView.count(
+        primary: false,
+        padding: const EdgeInsets.all(20),
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+        crossAxisCount: 2,
+        children: <Widget> [
+          Container(
+            padding: const EdgeInsets.all(8),
+            color: Colors.teal[100],
+            child: const Text("He'd have you all unravel at the"),
           ),
-        ),
-      ),
-    );
+          Container(
+            padding: const EdgeInsets.all(8),
+            color: Colors.teal[200],
+            child: const Text('Heed not the rabble'),
+          ),
+          Container(
+            padding: const EdgeInsets.all(8),
+            color: Colors.teal[300],
+            child: const Text('Sound of screams but the'),
+          ),
+          Container(
+            padding: const EdgeInsets.all(8),
+            color: Colors.teal[400],
+            child: const Text('Who scream'),
+          ),
+          Container(
+            padding: const EdgeInsets.all(8),
+            color: Colors.teal[500],
+            child: const Text('Revolution is coming...'),
+          ),
+          Container(
+            padding: const EdgeInsets.all(8),
+            color: Colors.teal[600],
+            child: const Text('Revolution, they...'),
+          ),
+        ],
+      )
+    ));
   }
 }
-
-Tile tile = new Tile(
-    image: '/home/angelique_mrcn/Documents/AMSE/tp2opy/assets/images/pic2.jpg', alignment: Alignment(0, 0));
-
-class DisplayTileWidget extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Display a Tile as a Cropped Image'),
-        centerTitle: true,
-      ),
-      body: Center(
-          child: Column(children: [
-        SizedBox(
-            width: 150.0,
-            height: 150.0,
-            child: Container(
-                margin: EdgeInsets.all(20.0),
-                child: this.createTileWidgetFrom(tile))),
-        Container(
-            height: 200,
-            child: Image.network('/home/angelique_mrcn/Documents/AMSE/tp2opy/assets/images/pic2.jpg',
-                fit: BoxFit.cover))
-      ])),
-    );
-  }
-
-  Widget createTileWidgetFrom(Tile tile) {
-    return InkWell(
-      child: tile.croppedImageTile(),
-      onTap: () {
-        print("tapped on tile");
-      },
-    );
-  }
-}
-*/
