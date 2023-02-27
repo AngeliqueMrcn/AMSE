b7479f5088548a1229653e3c79c7b8730c9f833d
Angelique_Mrcn <angelique64.merceron@gmail.com>
2023-02-27 21:21:43 +0100

    partie 1 exo5

--- tp2/lib/exo5.dart
+++ tp2/lib/exo5.dart
@@ -1,39 +1,28 @@
-GridView.count(
-  primary: false,
-  padding: const EdgeInsets.all(20),
-  crossAxisSpacing: 10,
-  mainAxisSpacing: 10,
-  crossAxisCount: 2,
-  children: <Widget>[
-    Container(
-      padding: const EdgeInsets.all(8),
-      color: Colors.teal[100],
-      child: const Text("He'd have you all unravel at the"),
-    ),
-    Container(
-      padding: const EdgeInsets.all(8),
-      color: Colors.teal[200],
-      child: const Text('Heed not the rabble'),
-    ),
-    Container(
-      padding: const EdgeInsets.all(8),
-      color: Colors.teal[300],
-      child: const Text('Sound of screams but the'),
-    ),
-    Container(
-      padding: const EdgeInsets.all(8),
-      color: Colors.teal[400],
-      child: const Text('Who scream'),
-    ),
-    Container(
-      padding: const EdgeInsets.all(8),
-      color: Colors.teal[500],
-      child: const Text('Revolution is coming...'),
-    ),
-    Container(
-      padding: const EdgeInsets.all(8),
-      color: Colors.teal[600],
-      child: const Text('Revolution, they...'),
-    ),
-  ],
-)
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Plateau de tuiles',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Plateau de tuiles'),
+        ),
+        body: GridView.count(
+          crossAxisCount: 4,
+          children: List.generate(
+            24,
+            (index) {
+              return Container(
+                color: Colors.blueGrey[index % 8 * 100],
+              );
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
--- tp2opy/lib/main.dart
+++ tp2opy/lib/main.dart
@@ -1,55 +1,28 @@
 import 'package:flutter/material.dart';
-import 'dart:math';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home : Scaffold(
-      appBar: AppBar(
-          title: Text('Display image'),
+    return MaterialApp(
+      title: 'Plateau de tuiles',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Plateau de tuiles'),
         ),
-      body : Text('Display image'),
-      GridView.count(
-        primary: false,
-        padding: const EdgeInsets.all(20),
-        crossAxisSpacing: 10,
-        mainAxisSpacing: 10,
-        crossAxisCount: 2,
-        children: <Widget> [
-          Container(
-            padding: const EdgeInsets.all(8),
-            color: Colors.teal[100],
-            child: const Text("He'd have you all unravel at the"),
+        body: GridView.count(
+          crossAxisCount: 4,
+          children: List.generate(
+            24,
+            (index) {
+              return Container(
+                color: Colors.blueGrey[index % 8 * 100],
+              );
+            },
           ),
-          Container(
-            padding: const EdgeInsets.all(8),
-            color: Colors.teal[200],
-            child: const Text('Heed not the rabble'),
-          ),
-          Container(
-            padding: const EdgeInsets.all(8),
-            color: Colors.teal[300],
-            child: const Text('Sound of screams but the'),
-          ),
-          Container(
-            padding: const EdgeInsets.all(8),
-            color: Colors.teal[400],
-            child: const Text('Who scream'),
-          ),
-          Container(
-            padding: const EdgeInsets.all(8),
-            color: Colors.teal[500],
-            child: const Text('Revolution is coming...'),
-          ),
-          Container(
-            padding: const EdgeInsets.all(8),
-            color: Colors.teal[600],
-            child: const Text('Revolution, they...'),
-          ),
-        ],
-      )
-    ));
+        ),
+      ),
+    );
   }
 }
